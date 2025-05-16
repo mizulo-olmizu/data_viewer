395ba7e8009482b7dc5e3c27a839a6c64a0e8b2c
* <*>
2025-05-17 01:15:48 +0900

    setup関数を切り出し

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,5 @@
 use polars::prelude::*;
-use tauri::{Manager, State};
+use tauri::{App, Manager, State};
 use tauri_plugin_cli::CliExt;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -14,40 +14,42 @@ struct AppData {
     data: DataFrame,
 }
 
+fn setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = match app.cli().matches() {
+        // `matches` here is a Struct with { args, subcommand }.
+        // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurrences }.
+        // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
+        Ok(matches) => matches
+            .args
+            .get("file_path")
+            .map(|arg_data| arg_data.value.clone()),
+        Err(_) => None,
+    }
+    .unwrap()
+    .as_str()
+    .map(|s| s.to_owned())
+    .unwrap();
+
+    println!("{}", file_path);
+
+    let data = CsvReadOptions::default()
+        .with_has_header(true)
+        .try_into_reader_with_file_path(Some(file_path.into()))
+        .unwrap()
+        .finish()
+        .unwrap();
+
+    app.manage(AppData { data });
+
+    Ok(())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_cli::init())
-        .setup(|app| {
-            let file_path = match app.cli().matches() {
-                // `matches` here is a Struct with { args, subcommand }.
-                // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurrences }.
-                // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
-                Ok(matches) => matches
-                    .args
-                    .get("file_path")
-                    .map(|arg_data| arg_data.value.clone()),
-                Err(_) => None,
-            }
-            .unwrap()
-            .as_str()
-            .map(|s| s.to_owned())
-            .unwrap();
-
-            println!("{}", file_path);
-
-            let data = CsvReadOptions::default()
-                .with_has_header(true)
-                .try_into_reader_with_file_path(Some(file_path.into()))
-                .unwrap()
-                .finish()
-                .unwrap();
-
-            app.manage(AppData { data });
-
-            Ok(())
-        })
+        .setup(setup)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
