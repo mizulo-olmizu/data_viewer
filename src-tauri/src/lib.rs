101911dc5962f20fde3ac1ae893b30503e19d210
* <*>
2025-05-17 17:37:39 +0900

    setup関数を修正してOption<DataFrame>を登録するように変更

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,27 +11,26 @@ fn greet(name: &str, state: State<'_, AppData>) -> String {
 }
 
 struct AppData {
-    data: DataFrame,
+    data: Option<DataFrame>,
 }
 
 fn setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
-    let file_path = app
-        .cli()
-        .matches()?
-        .args
+    let args = app.cli().matches()?.args;
+
+    let file_path = args
         .get("file_path")
-        .map(|arg_data| arg_data.value.clone())
-        .unwrap()
-        .as_str()
-        .map(|s| s.to_owned())
-        .unwrap();
+        .and_then(|arg_data| arg_data.value.as_str());
 
-    println!("{}", file_path);
+    println!("{:?}", file_path);
 
-    let data = CsvReadOptions::default()
-        .with_has_header(true)
-        .try_into_reader_with_file_path(Some(file_path.into()))?
-        .finish()?;
+    let data = file_path
+        .map(|file_path| {
+            CsvReadOptions::default()
+                .with_has_header(true)
+                .try_into_reader_with_file_path(Some(file_path.into()))
+                .and_then(|reader| reader.finish())
+        })
+        .transpose()?;
 
     app.manage(AppData { data });
 
@@ -43,7 +42,12 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_cli::init())
-        .setup(setup)
+        .setup(|app| {
+            if let Err(err) = setup(app) {
+                eprintln!("Error setting up app: {}", err);
+            };
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
