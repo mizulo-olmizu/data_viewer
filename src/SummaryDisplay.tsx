2894471bd16bcdcb05461b5f18eb3abed5c3a188
* <*>
2025-05-24 13:18:25 +0900

    SummaryDisplayをカード型に変更

--- src/SummaryDisplay.tsx
+++ src/SummaryDisplay.tsx
@@ -1,3 +1,6 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
 import { Summary } from "./types";
 
 export interface SummaryDisplayProps {
@@ -6,62 +9,72 @@ export interface SummaryDisplayProps {
 
 export default function SummaryDisplay({ summary }: SummaryDisplayProps) {
   return (
-    <div>
+    <Grid container spacing={2}>
       {summary.map((item, index) => {
         if (item.type == "numeric") {
           return (
-            <div key={index}>
-              <h3>Numeric Summary</h3>
-              <p>Column Name: {item.columnName}</p>
-              <p>Not Null Count: {item.notNullCount ?? "N/A"}</p>
-              <p>Null Count: {item.nullCount ?? "N/A"}</p>
-              <p>Min: {item.min ?? "N/A"}</p>
-              <p>Q1: {item.q1 ?? "N/A"}</p>
-              <p>Median: {item.median ?? "N/A"}</p>
-              <p>Q3: {item.q3 ?? "N/A"}</p>
-              <p>Max: {item.max ?? "N/A"}</p>
-              <p>Mean: {item.mean ?? "N/A"}</p>
-            </div>
+            <Grid>
+              <Card key={index}>
+                <CardContent>
+                  <h2>{item.columnName}</h2>
+                  <p>Not Null Count: {item.notNullCount ?? "N/A"}</p>
+                  <p>Null Count: {item.nullCount ?? "N/A"}</p>
+                  <p>Min: {item.min ?? "N/A"}</p>
+                  <p>Q1: {item.q1 ?? "N/A"}</p>
+                  <p>Median: {item.median ?? "N/A"}</p>
+                  <p>Q3: {item.q3 ?? "N/A"}</p>
+                  <p>Max: {item.max ?? "N/A"}</p>
+                  <p>Mean: {item.mean ?? "N/A"}</p>
+                </CardContent>
+              </Card>
+            </Grid>
           );
         }
 
         if (item.type == "categorical") {
           return (
-            <div key={index}>
-              <h3>Categorical Summary</h3>
-              <p>Column Name: {item.columnName}</p>
-              <p>Not Null Count: {item.notNullCount ?? "N/A"}</p>
-              <p>Null Count: {item.nullCount ?? "N/A"}</p>
-              <h4>Value Counts:</h4>
-              {item.valueCounts ? (
-                <ul>
-                  {item.valueCounts.map((vc, vcIndex) => (
-                    <li key={vcIndex}>
-                      Value: {vc.value}, Count: {vc.count ?? "N/A"}, Prop:{" "}
-                      {vc.prop ?? "N/A"}
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p>N/A</p>
-              )}
-            </div>
+            <Grid>
+              <Card key={index}>
+                <CardContent>
+                  <h2>{item.columnName}</h2>
+                  <p>Not Null Count: {item.notNullCount ?? "N/A"}</p>
+                  <p>Null Count: {item.nullCount ?? "N/A"}</p>
+                  <h3>Value Counts:</h3>
+                  {item.valueCounts ? (
+                    <ul>
+                      {item.valueCounts.map((vc, vcIndex) => (
+                        <li key={vcIndex}>
+                          Value: {vc.value}, Count: {vc.count ?? "N/A"}, Prop:{" "}
+                          {vc.prop ?? "N/A"}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p>N/A</p>
+                  )}
+                </CardContent>
+              </Card>
+            </Grid>
           );
         }
 
         if (item.type == "other") {
           return (
-            <div key={index}>
-              <h3>Other Summary</h3>
-              <p>Column Name: {item.columnName}</p>
-              <p>Not Null Count: {item.notNullCount ?? "N/A"}</p>
-              <p>Null Count: {item.nullCount ?? "N/A"}</p>
-            </div>
+            <Grid>
+              <Card key={index}>
+                <CardContent>
+                  <h2>{item.columnName}</h2>
+                  <p>Column Name: {item.columnName}</p>
+                  <p>Not Null Count: {item.notNullCount ?? "N/A"}</p>
+                  <p>Null Count: {item.nullCount ?? "N/A"}</p>
+                </CardContent>
+              </Card>
+            </Grid>
           );
         }
 
         return null;
       })}
-    </div>
+    </Grid>
   );
 }
